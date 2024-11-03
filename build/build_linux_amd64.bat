6f9a478d8778d8f9da6a2202aee3f276340cd40f
goodplayer <meidomx@gmail.com>
2024-11-03 12:28:11 +0800

    Add onlyagent in build scripts

--- build/build_linux_amd64.bat
+++ build/build_linux_amd64.bat
@@ -1,9 +1,11 @@
 
-del /f/q onlyconfig.exe
-del /f/q webmgr.exe
+del /f/q onlyconfig
+del /f/q webmgr
+del /f/q onlyagent
 
 set GOOS=linux
 set GOARCH=amd64
 
 go build -o onlyconfig ..
 go build -o webmgr ../cmd/webmgr
+go build -o onlyagent ../cmd/onlyagent
--- build/build_win_amd64.bat
+++ build/build_win_amd64.bat
@@ -1,9 +1,11 @@
 
 del /f/q onlyconfig.exe
 del /f/q webmgr.exe
+del /f/q onlyagent.exe
 
 set GOOS=windows
 set GOARCH=amd64
 
 go build -o onlyconfig.exe ..
 go build -o webmgr.exe ../cmd/webmgr
+go build -o onlyagent.exe ../cmd/onlyagent
